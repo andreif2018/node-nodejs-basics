c4416f55b15da743d0f93d8f168fb3fce3a193e8
Andrei <andreifrolov83@gmail.com>
2022-06-05 22:53:41 +0300

    feat: implement method in calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,10 @@
+import * as crypto from "crypto";
+import * as fileSystem from "fs";
+
 export const calculateHash = async () => {
-    // Write your code here 
+  await fileSystem.readFile('./files/fileToCalculateHashFor.txt', 'utf8', (error, fileContent) => {
+    if (error) throw error;
+    const result = crypto.createHash('sha256').update(fileContent);
+    console.log(result.digest('hex'));
+  });
 };
