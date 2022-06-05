106495b8513d1648500fe10f83c7bf109f0f11c7
Andrei <andreifrolov83@gmail.com>
2022-06-05 18:44:38 +0300

    feat: implement method rename of file system module subtask

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,20 @@
+import fileSystem from "fs";
+
 export const rename = async () => {
-    // Write your code here 
+  const sourcePath = './files/wrongFilename.txt';
+  const dirPath = './files/properFilename.md';
+  const errorMessage = 'FS operation failed';
+  await fileSystem.access(sourcePath, fileSystem.constants.F_OK, (error) => {
+    if (error) throw errorMessage;
+  });
+  await fileSystem.access(dirPath, fileSystem.constants.F_OK, (error) => {
+    if (error) {
+      fileSystem.rename(sourcePath, dirPath, (error) => {
+        if (error) throw errorMessage;
+      });
+    }
+    else {
+      throw errorMessage;
+    }
+  });
 };
