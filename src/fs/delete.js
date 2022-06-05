ff90a55a2008a62800bc6b4ee53eda9a54432234
Andrei <andreifrolov83@gmail.com>
2022-06-05 18:59:50 +0300

    feat: implement method remove of file system module subtask

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,9 @@
+import * as fileSystem from 'fs';
+
 export const remove = async () => {
-    // Write your code here 
+  const filePath = './files/fileToRemove.txt';
+  const errorMessage = 'FS operation failed';
+  await fileSystem.unlink(filePath, (error) => {
+    if (error) throw errorMessage;
+  });
 };
