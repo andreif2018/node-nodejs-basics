b3f1b26a24761803055aa0643ad7f2ec47c49453
Andrei <andreifrolov83@gmail.com>
2022-06-05 18:35:32 +0300

    feat: implement method copy of file system module subtask

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,22 @@
+import * as fileSystem from 'fs';
+
 export const copy = async () => {
-    // Write your code here 
+  const sourcePath = './files/';
+  const dirPath = './files_copy/';
+  const errorMessage = 'FS operation failed';
+  await fileSystem.access(sourcePath, fileSystem.constants.F_OK, (error) => {
+    if (error) throw errorMessage;
+  });
+  await fileSystem.mkdir(dirPath,{ recursive: false }, (error) => {
+    if (error) throw errorMessage;
+    fileSystem.readdir(sourcePath, (error, filNames) => {
+      if (error) throw errorMessage;
+      filNames.forEach((fileName) => {
+        fileSystem.copyFile(sourcePath + fileName, dirPath + fileName, (error) => {
+          if (error) throw error;
+          console.log(`${sourcePath + fileName} was copied to ${dirPath + fileName}`);
+        });
+      })
+    });
+  });
 };
