7bc8339677654d12fb445cce5507a75fb739d398
Andrei <andreifrolov83@gmail.com>
2022-06-05 19:09:32 +0300

    feat: implement method read of file system module subtask

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,10 @@
+import fileSystem from "fs"
+
 export const read = async () => {
-    // Write your code here 
+  const filePath = './files/fileToRead.txt';
+  const errorMessage = 'FS operation failed';
+  fileSystem.readFile(filePath, 'utf8', (error, fileContent) => {
+    if (error) throw errorMessage;
+    console.log(fileContent);
+  });
 };
