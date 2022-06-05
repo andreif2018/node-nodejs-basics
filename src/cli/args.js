8f18e109938e287da453e8e3994c0884daf8321f
Andrei <andreifrolov83@gmail.com>
2022-06-05 22:01:39 +0300

    feat: implement method parseArgs of cli module subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,21 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const propNames = [];
+  const propValues = [];
+  const result = [];
+  let firstParam;
+  process.argv.forEach((item, index) => {
+    if (item.startsWith('--')) {
+      propNames.push(item);
+      if (!firstParam) firstParam = index;
+    }
+  });
+  process.argv.slice(firstParam).forEach((item) => {
+    if (!item.startsWith('--')) {
+      propValues.push(item);
+    }
+  });
+  propNames.forEach((item, index) => {
+    result.push(`${item.slice(2)} is ${propValues[index]}`);
+  })
+  console.log(result.join(', '));
 };
