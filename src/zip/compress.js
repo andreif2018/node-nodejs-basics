2acc20bc16d8dfe6676c6c2b083c4df949652ab4
Andrei <andreifrolov83@gmail.com>
2022-06-06 10:21:55 +0300

    feat: implement method in compress.js of Zlib module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,9 @@
+import * as zip from "zlib";
+import * as fileSystem from 'fs';
+
 export const compress = async () => {
-    // Write your code here 
+  const handleZip = zip.createGzip();
+  const sourceFile = fileSystem.createReadStream('./files/fileToCompress.txt');
+  const resultFile = fileSystem.createWriteStream('./files/archive.gz');
+  await sourceFile.pipe(handleZip).pipe(resultFile);
 };
