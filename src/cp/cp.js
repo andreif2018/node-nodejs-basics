633b0240075366f305564f88822744a4c50a5e90
Andrei <andreifrolov83@gmail.com>
2022-06-06 12:26:07 +0300

    feat: implement methods in cp.js script.js in for cp module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,9 @@
+import { fork } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProc = fork('./files/script.js', args);
+  childProc.send(process.stdin);
+  childProc.on('message', (content) => {
+    process.stdout.write('Printing received data from child process:' + content);
+  });
 };
--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -7,6 +7,7 @@ const echoInput = (chunk) => {
     const chunkStringified = chunk.toString();
     if (chunkStringified.includes('CLOSE')) process.exit(0);
     process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+    process.send(`send updated chunk to master process: UPD_${chunk.toString()}\n`)
 };
 
 process.stdin.on('data', echoInput);
