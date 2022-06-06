137ee7883639acef23bde6e4f688c98b196b4927
Andrei <andreifrolov83@gmail.com>
2022-06-06 10:24:31 +0300

    feat: implement method in decompress.js of Zlib module

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,9 @@
+import zip from "zlib";
+import fileSystem from "fs";
+
 export const decompress = async () => {
-    // Write your code here 
+  const handleZip = zip.createUnzip();
+  const sourceFile = fileSystem.createReadStream('./files/archive.gz');
+  const resultFile = fileSystem.createWriteStream('./files/fileToCompress.txt');
+  await sourceFile.pipe(handleZip).pipe(resultFile);
 };
