db83c9442170d4e15bdec3a31ad0fe6967ef74c7
Andrei <andreifrolov83@gmail.com>
2022-06-06 09:44:26 +0300

    feat: implement method in transform.js of stream module

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,12 @@
+import { Transform } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+
+  const trStream = new Transform ({
+    transform(chunk, encoding, callback) {
+      this.push(chunk.toString().split('').reverse().join(''));
+      callback();
+    }
+  });
+  await process.stdin.pipe(trStream).pipe(process.stdout);
 };
