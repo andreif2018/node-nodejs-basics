8d044620ab280666ec935443c439ecf5fb6a73a3
Andrei <andreifrolov83@gmail.com>
2022-06-06 16:11:08 +0300

    feat: implement main.js of thread module subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,20 @@
+import {Worker} from "worker_threads";
+import * as operationSystem from 'os'
+
 export const performCalculations = async () => {
-    // Write your code here
+  const numberOfWorkers = operationSystem.cpus().length;
+  const result = [];
+  for (let index = 10; index < 10 + numberOfWorkers; index++) {
+    console.log("send to worker number: ", index);
+    const worker = new Worker('./worker.js');
+    worker.postMessage(index);
+    worker.on('message', (data) => {
+      result.push({ status: 'resolved', data: data });
+    });
+    worker.on('error', error => {
+      result.push({ status: 'resolved', data: null });
+    });
+  }
+  setTimeout(() => console.log(result), 3000);
+  return result;
 };
