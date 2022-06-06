7bd0e51c7008ae1928f31c6dec03ac610be7a5d7
Andrei <andreifrolov83@gmail.com>
2022-06-06 15:23:23 +0300

    refactor: update worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,22 +1,11 @@
-import { Worker, parentPort } from 'worker_threads';
-const message = 'message';
+import { parentPort } from 'worker_threads';
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const worker = new Worker('./worker.js');
-worker.postMessage(Math.floor(Math.random() * 10));
-worker.on(message, (result) => {
-  console.log(result);
-});
-
 export const sendResult = () => {
   // This function sends result of nthFibonacci computations to main thread
-  parentPort.on(message, (inputData) => {
-    parentPort.postMessage(
-        inputData.map(content => {
-          content = JSON.parse(content);
-          return nthFibonacci(content);
-        }));
+  parentPort.on('message', (digit) => {
+    parentPort.postMessage(nthFibonacci(digit));
   });
 };
 
