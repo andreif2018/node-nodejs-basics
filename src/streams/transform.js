83b4fb3b207390e8f6e8117539233dbeffbe09c9
Andrei Frolov <andreifrolov83@gmail.com>
2023-06-20 07:05:54 +0200

    feat: implement stream module

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const trStream = new Transform ({
+        transform(chunk, encoding, callback) {
+          this.push(chunk.toString().split('').reverse().join(''));
+          callback();
+        }
+      });
+      await process.stdin.pipe(trStream).pipe(process.stdout);
 };
 
 await transform();
