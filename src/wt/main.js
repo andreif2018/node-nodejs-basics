07b22c2b5157cf25715aadbed60690fb601ff020
Andrei Frolov <andreifrolov83@gmail.com>
2023-06-20 07:17:25 +0200

    feat: implement worker threads module

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,22 @@
+import {Worker} from "worker_threads";
+import * as operationSystem from 'os'
+
 const performCalculations = async () => {
-    // Write your code here
+    const numberOfWorkers = operationSystem.cpus().length;
+  const result = [];
+  for (let index = 10; index < 10 + numberOfWorkers; index++) {
+    console.log("send to worker number: ", index);
+    const worker = new Worker('./worker.js');
+    worker.postMessage(index);
+    worker.on('message', (data) => {
+      result.push({ status: 'resolved', data: data });
+    });
+    worker.on('error', error => {
+      result.push({ status: 'resolved', data: null });
+    });
+  }
+  setTimeout(() => console.log(result), 3000);
+  return result;
 };
 
 await performCalculations();
