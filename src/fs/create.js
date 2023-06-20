4e57e57c60309a94017fd64bb94485ad8e2cf715
Andrei Frolov <andreifrolov83@gmail.com>
2023-06-20 06:30:40 +0200

    feat: implement fs module

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import * as fileSystem from 'fs';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = './files/fresh.txt';
+  const fileContent = 'I am fresh and young';
+  const errorMessage = 'FS operation failed';
+  await fileSystem.access(filePath, fileSystem.constants.F_OK, (error) => {
+    if (error) {
+      fileSystem.appendFile(filePath, fileContent, (error) => {
+        if (error) {
+          throw new Error(errorMessage);
+        }
+        else {
+          console.log(`File ${filePath.slice(-10)} has been created with content: ${fileContent}`);
+        }
+      });
+    } else {
+      throw new Error(errorMessage);
+    }
+  });
 };
 
 await create();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,12 @@
+import * as fileSystem from 'fs';
+
 const list = async () => {
-    // Write your code here 
+    const filePath = './files/';
+    const errorMessage = 'FS operation failed';
+    await fileSystem.readdir(filePath, (error, filNames) => {
+      if (error) throw errorMessage;
+      filNames.forEach((fileName) => console.log(fileName));
+    });
 };
 
 await list();
